a6352921cbdb31c38d4fc8d223cf660e1ed60815
DC00497892 <DC00497892@INNOSZLP00466.TechMahindra.com>
2020-04-18 14:24:31 +0530

    RLS Excel view  file

---
+++ src/main/java/com/techm/views/RLSExcelView.java
@@ -0,0 +1,184 @@
+package com.techm.views;
+
+
+//Excel View Pojo
+public class RLSExcelView {
+	private String projectPhase;
+	private String vertical;
+	private String tower;
+	private String subTower;
+	private String role;
+    private String billable;
+    private String workPlace;
+    private String location;
+    private String city;
+    private String skillType;
+    private String resourceType;
+    private String band;
+    private String premises;
+    private String remarks;
+    private String daysYear1;
+    private String daysYear2;
+    private String daysYear3;
+    private String daysYear4;
+    private String daysYear5;
+    private String daysYear6;
+    private String daysYear7;
+    private String daysYear8;
+    private String daysYear9;
+    private String daysYear10;
+    private int total;
+    
+	public String getProjectPhase() {
+		return projectPhase;
+	}
+	public void setProjectPhase(String projectPhase) {
+		this.projectPhase = projectPhase;
+	}
+	public String getVertical() {
+		return vertical;
+	}
+	public void setVertical(String vertical) {
+		this.vertical = vertical;
+	}
+	public String getTower() {
+		return tower;
+	}
+	public void setTower(String tower) {
+		this.tower = tower;
+	}
+	public String getSubTower() {
+		return subTower;
+	}
+	public void setSubTower(String subTower) {
+		this.subTower = subTower;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getBillable() {
+		return billable;
+	}
+	public void setBillable(String billable) {
+		this.billable = billable;
+	}
+	public String getWorkPlace() {
+		return workPlace;
+	}
+	public void setWorkPlace(String workPlace) {
+		this.workPlace = workPlace;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getSkillType() {
+		return skillType;
+	}
+	public void setSkillType(String skillType) {
+		this.skillType = skillType;
+	}
+	public String getResourceType() {
+		return resourceType;
+	}
+	public void setResourceType(String resourceType) {
+		this.resourceType = resourceType;
+	}
+	public String getBand() {
+		return band;
+	}
+	public void setBand(String band) {
+		this.band = band;
+	}
+	public String getPremises() {
+		return premises;
+	}
+	public void setPremises(String premises) {
+		this.premises = premises;
+	}
+	public String getRemarks() {
+		return remarks;
+	}
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	public String getDaysYear1() {
+		return daysYear1;
+	}
+	public void setDaysYear1(String daysYear1) {
+		this.daysYear1 = daysYear1;
+	}
+	public String getDaysYear2() {
+		return daysYear2;
+	}
+	public void setDaysYear2(String daysYear2) {
+		this.daysYear2 = daysYear2;
+	}
+	public String getDaysYear3() {
+		return daysYear3;
+	}
+	public void setDaysYear3(String daysYear3) {
+		this.daysYear3 = daysYear3;
+	}
+	public String getDaysYear4() {
+		return daysYear4;
+	}
+	public void setDaysYear4(String daysYear4) {
+		this.daysYear4 = daysYear4;
+	}
+	public String getDaysYear5() {
+		return daysYear5;
+	}
+	public void setDaysYear5(String daysYear5) {
+		this.daysYear5 = daysYear5;
+	}
+	public String getDaysYear6() {
+		return daysYear6;
+	}
+	public void setDaysYear6(String daysYear6) {
+		this.daysYear6 = daysYear6;
+	}
+	public String getDaysYear7() {
+		return daysYear7;
+	}
+	public void setDaysYear7(String daysYear7) {
+		this.daysYear7 = daysYear7;
+	}
+	public String getDaysYear8() {
+		return daysYear8;
+	}
+	public void setDaysYear8(String daysYear8) {
+		this.daysYear8 = daysYear8;
+	}
+	public String getDaysYear9() {
+		return daysYear9;
+	}
+	public void setDaysYear9(String daysYear9) {
+		this.daysYear9 = daysYear9;
+	}
+	public String getDaysYear10() {
+		return daysYear10;
+	}
+	public void setDaysYear10(String daysYear10) {
+		this.daysYear10 = daysYear10;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+    
+    
+       }
