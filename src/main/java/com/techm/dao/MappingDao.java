cfb97de0ee4682c23f5d66ae858a612d2b1b322c
Prem <prem.ghorai09@gmail.com>
2020-05-07 20:45:43 +0530

    ok

--- src/main/java/com/techm/dao/MappingDao.java
+++ src/main/java/com/techm/dao/MappingDao.java
@@ -9,6 +9,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.techm.bean.MappingRole;
+import com.techm.bean.RoleAccess;
+import com.techm.entity.DealSpecs;
 
 @Component
 public class MappingDao {
@@ -17,6 +19,7 @@ public class MappingDao {
 	
 	private final String SQL_FIND_role = "SELECT DISTINCT uinfo.LOGIN_ID, mroles.ROLE, mmod.MODULE, mar.ACCESS_RIGHT FROM t_user_info uinfo LEFT JOIN t_user_role tur ON tur.USER_ID = uinfo.USER_SRNO LEFT JOIN m_roles mroles ON mroles.ROLE_ID = tur.ROLE_ID LEFT JOIN t_user_role_access racc ON racc.ROLE_ID = mroles.ROLE_ID LEFT JOIN m_module mmod ON racc.Module_id = mmod.MODULE_ID LEFT JOIN m_access_right mar ON mar.ACCESS_RIGHT_ID = racc.ROLE_ACCESS_ID WHERE uinfo.LOGIN_ID = ?";
 	
+	private final String SQL_role_access="SELECT mr.role,mm.module,mar.access_right,tura.role_access_id,tura.module_id,tura.`ROLE_ID` FROM `t_user_role_access` tura INNER JOIN `m_roles` mr ON mr.role_id=tura.role_id AND mr.record_status<>'D' INNER JOIN `m_module` mm ON mm.module_id=tura.module_id AND mm.record_status<>'D' INNER JOIN `m_access_right` mar ON mar. access_right_id=tura.role_access_id AND mar.record_status<>'D'  WHERE tura.record_status<>'D' AND tura.`ROLE_ID`=?";
 	
 	@Autowired
 	public MappingDao(DataSource dataSource) {
@@ -27,7 +30,19 @@ public class MappingDao {
 		
 		List<MappingRole> role=jdbcTemplate.query(SQL_FIND_role, new RoleMappingMaper(), id);
 		return role;
-		//jdbcTemplate.queryForObject(SQL_FIND_role, new Object[] { id }, new RoleMappingMaper());
+		
+	}
+	
+	public List<RoleAccess> getRoleAccessById(String id) {
+		
+		List<RoleAccess> roleAccess=jdbcTemplate.query(SQL_role_access, new RoleAccessMappingMaper(), id);
+		return roleAccess;
+		
+	}
+	
+	public int addRoleAccess(int accessRight,int role,int module) {
+	    return jdbcTemplate.update(
+	      "INSERT INTO t_user_role_access (ROLE_ACCESS_ID, ROLE_ID,RECORD_STATUS,Module_id) VALUES (?, ?, ?, ?)", accessRight, role, "C", module);
 	}
 	
 	
